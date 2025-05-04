cdccb5347e1b2a5a19523b39d2d27c99685870f9
Eric Dahle <ewdahle@gmail.com>
2025-05-04 17:01:38 -0400

    Added image assets for dice.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,6 +38,12 @@ fun DiceWithButtonAndImage (modifier: Modifier = Modifier) {
     Column (
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {}
+    ) {
+        Button(onClick = {/*TODO*/}){
+            Text(
+                stringResource(R.string.roll)
+            )
+        }
+    }
 }
 
